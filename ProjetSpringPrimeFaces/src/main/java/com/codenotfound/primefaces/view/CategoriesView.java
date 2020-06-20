178faea11767830fcf81554d63362ad6a8392eb3
moimouad <mouad04km@gmail.com>
2020-06-20 22:19:39 +0100

    admin_categ

--- ProjetSpringPrimeFaces/src/main/java/com/codenotfound/primefaces/view/CategoriesView.java
+++ ProjetSpringPrimeFaces/src/main/java/com/codenotfound/primefaces/view/CategoriesView.java
@@ -4,13 +4,19 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.codenotfound.primefaces.Context;
+import com.codenotfound.primefaces.model.Account;
 import com.codenotfound.primefaces.model.Category;
+import com.codenotfound.primefaces.repository.AccountRepository;
 import com.codenotfound.primefaces.repository.CategoryRepository;
 
 @Named
@@ -24,7 +30,15 @@ public class CategoriesView implements Serializable {
 
 	  private List<Category> categories;
 	  
-	  @PostConstruct
+	  private Category selected;
+	  
+	public Category getSelected() {
+		return selected;
+	}
+	public void setSelected(Category selected) {
+		this.selected = selected;
+	}
+	@PostConstruct
 	  public void init() {
 		  categories = categoriesRepository.findAll();
 	  }
@@ -41,6 +55,53 @@ public class CategoriesView implements Serializable {
 				System.out.println(e);
 			}
 		}
+	  
+	  public void onRowSelect(SelectEvent event) {
+	        this.selected = (Category) event.getObject();
+	        FacesContext context = FacesContext.getCurrentInstance();
+
+	        if (this.selected == null) {
+	            String msg = "No Category is selected!!";
+	            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, null));
+	        } else {
+	            System.out.println("Gnome is selected in userBean");
+	            String msg = "Categoty selected: " + this.selected.getName();
+	            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null));
+	        }
+
+	    }
+	  
+	    public void onRowUnselect(UnselectEvent event) {
+	        this.selected = null;
+	        String msg = "Gnome unselected: " + this.selected;
+	        FacesContext context = FacesContext.getCurrentInstance();
+	        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null));
+	        if (this.selected == null) {
+	            System.out.println(this.selected + ": is unselected");
+	        }
+	    }
+	    
+	    public void deleteExistingProduct() {
+	        System.out.println("Deleting Existing product... ");
+	        Category gnome = selected;
+	        FacesContext context = FacesContext.getCurrentInstance();
+	        // need to handel when gnome is null
+	        if (gnome == null) {
+	            System.out.println("Selected gnome is null");
+	            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Admin: Selected product to be deleted is null. Bug!!! in the product list.", null));
+	        }
+
+	        // call EJB to delete the product
+	        try {
+	        	CategoryRepository accRepo = Context.getContext().getBean(CategoryRepository.class);
+			    accRepo.delete(gnome.getId());
+	            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Admin: Product deleted successfully.", null));
+	            redirect("admin_categ.xhtml");
+	        } catch (Exception e) {
+	            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Admin: Product deletion was unsuccessful.", null));
+	        }
+
+	    }
 		  
 
 }
