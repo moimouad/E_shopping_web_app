f228cc809e99d1173bd52af71ac2061883709b07
moimouad <mouad04km@gmail.com>
2020-06-21 16:03:57 +0100

    img

--- ProjetSpringPrimeFaces/src/main/java/com/codenotfound/primefaces/view/ImagesView.java
+++ ProjetSpringPrimeFaces/src/main/java/com/codenotfound/primefaces/view/ImagesView.java
@@ -16,7 +16,7 @@ public class ImagesView {
     @PostConstruct
     public void init() {
         images = new ArrayList<String>();
-        for (int i = 1; i <= 12; i++) {
+        for (int i = 1; i <= 4; i++) {
             images.add("nature" + i + ".jpg");
         }
     }
